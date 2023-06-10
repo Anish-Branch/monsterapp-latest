0dca78871da7509c70e45c6b9d4f57e5e53982b9
Anish-Branch <95915369+Anish-Branch@users.noreply.github.com>
2023-06-10 09:26:58 +0100

    Initial Commit

---
+++ monsterapp/monsterappApp.swift
@@ -0,0 +1,17 @@
+//
+//  monsterappApp.swift
+//  monsterapp
+//
+//  Created by Anish Somani on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct monsterappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
